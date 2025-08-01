65a7e5a17eca4642c081aaeb62e181f0e823f72c
Lyo Nya Zhee-Zhin <lazhizhin@yandex.ru>
2025-08-01 15:34:49 +0200

    Added eza, replacement for ls

--- user/shell/default.nix
+++ user/shell/default.nix
@@ -1,30 +1,40 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [ ./tmux ];
-  programs.fish = {
-    enable = true;
-    shellInit = "set -U fish_greeting";
-    generateCompletions = true;
-    functions = {
-      tc = "tmux new-session -s $(basename $PWD)";
+  home.packages = [ pkgs.uv ];
+  programs = {
+    eza = {
+      enable = true;
+      enableFishIntegration = true;
+      git = true;
+      icons = "always";
     };
-    shellAbbrs = {
-      # Git
-      ga = "git add";
-      gst = "git status";
-      gc = "git commit";
-      gp = "git push";
-      gd = "git diff";
-      # Tmux
-      ta = "tmux attach -t";
-      tls = "tmux ls";
-      td = "tmux kill-session -t";
-      # Nix
-      nrb = "sudo nixos-rebuild switch --flake ~/.files --show-trace";
-      ngc = "sudo nix-collect-garbage -d && sudo nixos-rebuild boot --flake ~/.files --show-trace";
-      # Uv
-      ur = "uv run";
-      uri = "uv run -m IPython";
+    fish = {
+      enable = true;
+      shellInit = "set -U fish_greeting";
+      generateCompletions = true;
+      functions = {
+        tc = "tmux new-session -s $(basename $PWD)";
+      };
+      shellAbbrs = {
+        # Git
+        ga = "git add";
+        gst = "git status";
+        gc = "git commit";
+        gp = "git push";
+        gd = "git diff";
+        gl = "git pull";
+        # Tmux
+        ta = "tmux attach -t";
+        tls = "tmux ls";
+        td = "tmux kill-session -t";
+        # Nix
+        nrb = "sudo nixos-rebuild switch --flake ~/.files --show-trace";
+        ngc = "sudo nix-collect-garbage -d && sudo nixos-rebuild boot --flake ~/.files --show-trace";
+        # Uv
+        ur = "uv run";
+        uri = "uv run -m IPython";
+      };
     };
   };
 }
